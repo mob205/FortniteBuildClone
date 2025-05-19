0f2502610e3a4c630b02e2299ad60f16d0586c89
mob205 <mobslayer205@gmail.com>
2025-05-18 20:55:53 -0400

    Add floor edit meshes

--- Source/FortniteBuildClone/Private/AbilitySystem/Abilities/Edit/FourTileEditTargetingActor.cpp
+++ Source/FortniteBuildClone/Private/AbilitySystem/Abilities/Edit/FourTileEditTargetingActor.cpp
@@ -24,32 +24,23 @@ static constexpr FBitGrid RotateClockwise(FBitGrid InBitGrid)
 
 void AFourTileEditTargetingActor::SetSelectedEdit(int32 InBitfield)
 {
-	GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Red, FString::Printf(TEXT("Bitfield is %d."), InBitfield));
-
 	// Every configuration, except 0, is a valid edit for all four-tile structures
 	if (InBitfield == 0)
 	{
-		GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Red, FString::Printf(TEXT("0 is not allowed - returning")));
 		return;
 	}
 
+	// Rotate to find the right orientation, since edit map only defines one orientation per configuration
 	int i{};
 	for (; i < 4 && !EditMap->Contains(InBitfield); ++i)
 	{
 		InBitfield = RotateClockwise(InBitfield).BitField;
-		GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Red, FString::Printf(TEXT("Switching to %d"), InBitfield));
 	}
-
-	if (!EditMap->Contains(InBitfield))
-	{
-		GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Red, FString::Printf(TEXT("Bitfield %d not in map!"), InBitfield));
-	}
+	
 	// This should be a valid edit
+	// Rotate structure opposite way of bitfield rotation to retain selection orientation 
 	const float AngleRotated = -90 * i;
 	AddActorWorldRotation({0, AngleRotated, 0});
 	
-	GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Red, FString::Printf(TEXT("Rotating %f degrees CW"), AngleRotated));
-
-
 	Super::SetSelectedEdit(InBitfield);
 }
